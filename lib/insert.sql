7242a5f60fd83827de153c745148dcfdda051e12
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-19 15:21:43 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,121 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Alpine Lodge', 'Recreation', 100000, '6/1/16', '12/1/16');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Banana Pies', 'Food', 200000, '9/1/16', '12/1/16');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Camel Racing', 'Recreation', 500000, '12/1/16', '12/1/17');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Duck Breeding', 'Book', 50000, '6/15/16', '8/15/16');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Elephant Ears', 'Music', 25000, '7/15/16', '12/15/16');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Franks on a Stick', 'Food', 1000000, '6/1/16', '6/1/17');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Goose in a Box', 'Animals', 5000, '4/1/16', '11/1/16');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Hall of Fame', 'Music', 100000, '6/1/16', '12/1/16');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('I Wrote a book on SQL', 'Book', 5000, '9/1/16', '10/1/16');
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Just kidding, I didn't", 'Book', 6000, '10/1/16', '12/1/16');
+
+INSERT INTO users (name, age) VALUES
+('Finnebar', 17);
+
+INSERT INTO users (name, age) VALUES
+('Bear', 6);
+
+INSERT INTO users (name, age) VALUES
+('Iguana', 4);
+
+INSERT INTO users (name, age) VALUES
+('Alex', 33);
+
+INSERT INTO users (name, age) VALUES
+('Amanda', 24);
+
+INSERT INTO users (name, age) VALUES
+('Sophie', 24);
+
+INSERT INTO users (name, age) VALUES
+('Rosey', 9);
+
+INSERT INTO users (name, age) VALUES
+('Victoria', 23);
+
+INSERT INTO users (name, age) VALUES
+('Franz', 100);
+
+INSERT INTO users (name, age) VALUES
+('Hermione', 30);
+
+INSERT INTO users (name, age) VALUES
+('Voldemort', 90);
+
+INSERT INTO users (name, age) VALUES
+('Marisa', 24);
+
+INSERT INTO users (name, age) VALUES
+('Swizzle', 4);
+
+INSERT INTO users (name, age) VALUES
+('Sirius', 36);
+
+INSERT INTO users (name, age) VALUES
+('Albus', 113);
+
+INSERT INTO users (name, age) VALUES
+('Squid', 5);
+
+INSERT INTO users (name, age) VALUES
+('Whale', 6);
+
+INSERT INTO users (name, age) VALUES
+('Pacha', 5);
+
+INSERT INTO users (name, age) VALUES
+('Ena', 24);
+
+INSERT INTO users (name, age) VALUES
+('Katie', 24);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+(10.00, 1, 2),
+(20.00, 1, 3),
+(40.00, 1, 4),
+(50.00, 2, 3),
+(10.00, 3, 2),
+(20.00, 4, 4),
+(40.00, 5, 10),
+(60.00, 6, 10),
+(50.00, 7, 9),
+(700.00, 8, 8),
+(1000.00, 8, 7),
+(40.00, 9, 6),
+(50.00, 9, 3),
+(50.00, 10, 4),
+(24.00, 12, 1),
+(34.00, 11, 1),
+(12.00, 13, 6),
+(19.00, 14, 5),
+(20.00, 15, 5),
+(40.00, 16, 6),
+(35.50, 17, 7),
+(40.00, 18, 8),
+(60.00, 19, 9),
+(70.00, 20, 10),
+(100.00, 20, 4),
+(40.00, 19, 1),
+(20.00, 18, 6),
+(90.00, 17, 9),
+(230.00, 16, 6),
+(450.00, 15, 5);
