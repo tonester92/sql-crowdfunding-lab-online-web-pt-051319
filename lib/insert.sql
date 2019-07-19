42d18fe638ccced1687a624b56be0d0451f76e8a
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-19 15:13:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,21 +1,92 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Alpine Lodge', 'Recreation', 100000, '6/1/16', '12/1/16');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Banana Pies', 'Food', 200000, '9/1/16', '12/1/16');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Camel Racing', 'Recreation', 500000, '12/1/16', '12/1/17');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Duck Breeding', 'Book', 50000, '6/15/16', '8/15/16');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Elephant Ears', 'Music', 25000, '7/15/16', '12/15/16');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Franks on a Stick', 'Food', 1000000, '6/1/16', '6/1/17');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Goose in a Box', 'Animals', 5000, '4/1/16', '11/1/16');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('Hall of Fame', 'Music', 100000, '6/1/16', '12/1/16');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ('I Wrote a book on SQL', 'Book', 5000, '9/1/16', '10/1/16');
+
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ("Just kidding, I didn't", 'Book', 6000, '10/1/16', '12/1/16');
+
+INSERT INTO users (name, age) VALUES
+('Finnebar', 17);
+
+INSERT INTO users (name, age) VALUES
+('Bear', 6);
+
+INSERT INTO users (name, age) VALUES
+('Iguana', 4);
+
+INSERT INTO users (name, age) VALUES
+('Alex', 33);
+
+INSERT INTO users (name, age) VALUES
+('Amanda', 24);
+
+INSERT INTO users (name, age) VALUES
+('Sophie', 24);
+
+INSERT INTO users (name, age) VALUES
+('Rosey', 9);
+
+INSERT INTO users (name, age) VALUES
+('Victoria', 23);
+
+INSERT INTO users (name, age) VALUES
+('Franz', 100);
+
+INSERT INTO users (name, age) VALUES
+('Hermione', 30);
+
+INSERT INTO users (name, age) VALUES
+('Voldemort', 90);
+
+INSERT INTO users (name, age) VALUES
+('Marisa', 24);
+
+INSERT INTO users (name, age) VALUES
+('Swizzle', 4);
+
+INSERT INTO users (name, age) VALUES
+('Sirius', 36);
+
+INSERT INTO users (name, age) VALUES
+('Albus', 113);
+
+INSERT INTO users (name, age) VALUES
+('Squid', 5);
+
+INSERT INTO users (name, age) VALUES
+('Whale', 6);
+
+INSERT INTO users (name, age) VALUES
+('Pacha', 5);
+
+INSERT INTO users (name, age) VALUES
+('Ena', 24);
+
+INSERT INTO users (name, age) VALUES
+('Katie', 24);
+
+
 
